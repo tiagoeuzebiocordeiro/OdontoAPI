5c348686ae11dcf650cdea756fc3fd7f6a1380bb
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-02-15 15:28:15 -0300

    Mapeamento de colunas para tabela banco de dados (database first)

--- OdontoAPI/DataContext/ApplicationDbContext.cs
+++ OdontoAPI/DataContext/ApplicationDbContext.cs
@@ -9,16 +9,17 @@ namespace OdontoAPI.DataContext
         {
         }
 
-        // Define os DbSets para suas entidades
+        // DbSets para suas entidades
         public DbSet<DentistaModel> Dentistas { get; set; }
         public DbSet<PacienteModel> Pacientes { get; set; }
         public DbSet<ConsultaModel> Consultas { get; set; }
 
 
-        //Meu banco já tá criado, então n preciso de Dbsets. só faço um mapeamento
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
+            /*COMEÇO DE MAPEAMENTO DOS DENTISTAS*/
             modelBuilder.Entity<DentistaModel>().ToTable("tb_dentistas").Property(d => d.Id).HasColumnName("id_dentista");
             modelBuilder.Entity<DentistaModel>().Property(d => d.Nome).HasColumnName("nome_dentista");
             modelBuilder.Entity<DentistaModel>().Property(d => d.CodigoCro).HasColumnName("cod_cro_dentista");
@@ -33,9 +34,32 @@ namespace OdontoAPI.DataContext
             modelBuilder.Entity<DentistaModel>().Property(d => d.Status).HasColumnName("status_dentista");
             modelBuilder.Entity<DentistaModel>().Property(d => d.DataCadastro).HasColumnName("data_cadastro_dentista");
             modelBuilder.Entity<DentistaModel>().Property(d => d.DataModificacao).HasColumnName("data_modificacao_dentista");
+            /*FIM DE MAPEAMENTO DOS DENTISTAS*/
 
-            modelBuilder.Entity<PacienteModel>().ToTable("tb_pacientes");
-            modelBuilder.Entity<ConsultaModel>().ToTable("tb_consultas");
+
+            /*COMEÇO DE MAPEAMENTO DOS PACIENTES*/
+            modelBuilder.Entity<PacienteModel>().ToTable("tb_pacientes").Property(p => p.Id).HasColumnName("id_paciente");
+            modelBuilder.Entity<PacienteModel>().Property(p => p.Nome).HasColumnName("nome_paciente");
+            modelBuilder.Entity<PacienteModel>().Property(p => p.DataNascimento).HasColumnName("data_nasc_paciente");
+            modelBuilder.Entity<PacienteModel>().Property(p => p.Cpf).HasColumnName("cpf_paciente");
+            modelBuilder.Entity<PacienteModel>().Property(p => p.Endereco).HasColumnName("endereco_paciente");
+            modelBuilder.Entity<PacienteModel>().Property(p => p.Telefone).HasColumnName("telefone_paciente");
+            modelBuilder.Entity<PacienteModel>().Property(p => p.Email).HasColumnName("email_paciente");
+            modelBuilder.Entity<PacienteModel>().Property(p => p.Sexo).HasColumnName("sexo_paciente");
+            modelBuilder.Entity<PacienteModel>().Property(p => p.Status).HasColumnName("status_paciente");
+            modelBuilder.Entity<PacienteModel>().Property(p => p.DataCadastro).HasColumnName("data_cadastro_paciente");
+            modelBuilder.Entity<PacienteModel>().Property(p => p.DataModificacao).HasColumnName("data_modificacao_paciente");
+            /*FIM DE MAPEAMENTO DOS PACIENTES*/
+
+            /*COMEÇO DE MAPEAMENTO DAS CONSULTAS*/
+            modelBuilder.Entity<ConsultaModel>().ToTable("tb_consultas").Property(c => c.Id).HasColumnName("id_consulta");
+            modelBuilder.Entity<ConsultaModel>().Property(c => c.DentistaAssociado).HasColumnName("id_dentista_associado");
+            modelBuilder.Entity<ConsultaModel>().Property(c => c.PacienteAssociado).HasColumnName("id_paciente_associado");
+            modelBuilder.Entity<ConsultaModel>().Property(c => c.DataConsulta).HasColumnName("data_consulta");
+            modelBuilder.Entity<ConsultaModel>().Property(c => c.DataCadastroConsulta).HasColumnName("data_cadastro_consulta");
+            modelBuilder.Entity<ConsultaModel>().Property(c => c.DataModificacaoConsulta).HasColumnName("data_modificacao_consulta");
+            /*FIM DE MAPEAMENTO DAS CONSULTAS*/
+
             base.OnModelCreating(modelBuilder);
         }
     }
