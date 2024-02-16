d19f15ddfad79f906b5c1afde20dee4ea9c3749e
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-02-16 18:18:52 -0300

    Impl: GetConsultas e CreateConsulta

---
+++ OdontoAPI/Controllers/ConsultaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OdontoAPI.Models;
+using OdontoAPI.Services.ConsultaService;
+
+namespace OdontoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsultaController : ControllerBase
+    {
+        private readonly IConsultaInterface _consultaInterface;
+        public ConsultaController(IConsultaInterface consultaInterface)
+        {
+            _consultaInterface = consultaInterface;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ConsultaModel>>> GetConsultas()
+        {
+            return Ok(await _consultaInterface.GetConsultas());
+        }
+
+    }
+}
--- OdontoAPI/Services/ConsultaService/ConsultaService.cs
+++ OdontoAPI/Services/ConsultaService/ConsultaService.cs
@@ -1,6 +1,60 @@
-﻿namespace OdontoAPI.Services.ConsultaService
+﻿using Microsoft.EntityFrameworkCore;
+using OdontoAPI.DataContext;
+using OdontoAPI.Models;
+
+namespace OdontoAPI.Services.ConsultaService
 {
     public class ConsultaService : IConsultaInterface
     {
+        private readonly ApplicationDbContext _context;
+
+        public ConsultaService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ServiceResponse<List<ConsultaModel>>> CreateConsulta(ConsultaModel consultaNova)
+        {
+            ServiceResponse<List<ConsultaModel>> serviceResponse = new ServiceResponse<List<ConsultaModel>>();
+            try
+            {
+                if (consultaNova == null)
+                {
+                    serviceResponse.Dados = null;
+                    serviceResponse.Mensagem = "Informe os dados!";
+                    serviceResponse.Sucesso = false;
+                    return serviceResponse;
+                }
+                _context.Consultas.Add(consultaNova);
+                await _context.SaveChangesAsync();
+                serviceResponse.Dados = _context.Consultas.ToList();
+            } catch (Exception ex)
+            {
+                serviceResponse.Mensagem = ex.Message;
+                serviceResponse.Sucesso = false;
+            }
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<ConsultaModel>>> GetConsultas()
+        {
+            ServiceResponse<List<ConsultaModel>> serviceResponse = new ServiceResponse<List<ConsultaModel>>();
+            try
+            {
+                serviceResponse.Dados = _context.Consultas.ToList();
+                if (serviceResponse.Dados.Count == 0)
+                {
+                    serviceResponse.Dados = null;
+                    serviceResponse.Mensagem = "Nenhum dado foi encontrado no banco de dados!";
+                    serviceResponse.Sucesso = false;
+                }
+
+            } catch (Exception ex)
+            {
+                serviceResponse.Mensagem = ex.Message;
+                serviceResponse.Sucesso = false;
+            }
+            return serviceResponse;
+        }
     }
 }
